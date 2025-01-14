095a5ab1db9264cd4b8540d32fc8586ef7b60cd1
Fields Marshall <fields.marshall@gmail.com>
2025-01-13 20:34:33 -0800

    updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,24 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
+
 
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(),mdx()],
+  content: {
+    collections: {
+      trips: {
+        schema: {
+          name: "string",
+          imgSrc: "string",
+          description: "string",
+          dates: "string",
+          price: "string",
+        },
+      },
+    },
+  },
   vite: {
     css: {
       preprocessorOptions: {
